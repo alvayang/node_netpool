3afb2bd9ba65be360465ee22ae6124100eb248a0
alvayang <netyang@gmail.com>
2011-07-13 21:28:34 +0800

    init respo

---
+++ test/bench.js
@@ -0,0 +1,68 @@
+/**
+ * @file   bench.js
+ * @author alvayang <alvayang@sohu-inc.com>
+ * @date   Wed Jul 13 21:14:38 2011
+ * 
+ * @brief  a client
+ * 
+ * 
+ */
+
+
+var carrier = require('./carrier');
+var poolmodule = require('../lib/pool');
+
+function logicb(){
+    var  i = 0;
+    while( i < 10 ){
+	(function(){
+	    pool.get_connection(function(connect){
+		console.log("logic b get connection:", connect.connection_index);
+		var carry = carrier.carry(connect);
+		carry.on('line', function doline(line){
+		    carry.removeListener('line', doline);
+		    console.log("in logic b:", line);
+		    pool.release(carry.reader);	    
+		    pool.status();
+		});
+		connect.write("bbb\n");
+	    }, true);
+	})();
+	i++;
+    }
+}
+
+function logica(){
+    var  i = 0;
+    while( i < 10 ){
+	(function(){
+	    pool.get_connection(function(connect){
+		console.log("logic a get connection:", connect.connection_index);
+		var carry = carrier.carry(connect);
+		carry.on('line', function doline(line){
+		    carry.removeListener('line', doline);
+		    console.log("in logic a : ", line);
+		    pool.release(carry.reader);
+		    pool.status();
+		});
+		connect.write("aaaa\n");
+	    }, true);
+	})();
+	i++;
+    }
+}
+
+//pool.init(main);
+function main(){
+    console.log("main called");
+    process.nextTick(function(){
+	logica();
+    });
+    process.nextTick(function(){
+	logicb();
+    });
+}
+var config = {port : 22222, host : '127.0.0.1', minsize : 1, maxsize : 2, callback : main};
+var pool = poolmodule.pool(config);
+
+//main();
---
+++ test/roster.js
@@ -0,0 +1,25 @@
+/**
+ * @file   roster.js
+ * @author alvayang <alvayang@sohu-inc.com>
+ * @date   Wed Jun 29 09:43:27 2011
+ * 
+ * @brief  提供TCP的服务.
+ *         A simple Echo Server.
+ *         the carrier component is comming from  https://github.com/pgte/carrier
+ */
+var net = require('net');
+var carrier = require('./carrier');
+
+(function(){
+    var server = net.createServer(function(socket) {
+	carrier.carry(socket, function(line){
+	    console.log('line:' + line);
+	    return socket.write(line + '\n');
+	});
+    });
+    server.listen(22222, function(){
+	console.log("server running at: tcp://0.22222");
+    });
+})();
+
+
