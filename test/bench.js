f5061a5893106a0c4cf3bd5b210068844056d515
alvayang <netyang@gmail.com>
2011-08-20 18:28:15 +0800

    fuck carrier the fucking module, fuck you

--- test/bench.js
+++ test/bench.js
@@ -32,27 +32,43 @@ function logicb(){
 }
 
 function logica(){
+	var timer = function(){
     var  i = 0;
-    while(i < 100000){
-	(function(){
+    while(i < 10000){
+	console.log('now:', i);
+
+	process.nextTick(function(){
+		//console.log('running');
 	    pool.get_connection(function(connect){
-		console.log("logic a get connection:", connect.connection_index);
+		//console.log("logic a get connection:", connect.connection_index);
+		connect.on('data', function(line){
+		});
+		/*
 		var carry = carrier.carry(connect);
 		carry.on('line', function doline(line){
 		    pool.release(carry.reader);
 		    pool.status();
 		    console.log("in logic a : ", line);
 		});
+		*/
+		try{
+			console.log('send');
 		connect.write("aaaa\n");
+		}catch(e){
+			console.log(e.message);
+		}
 	    }, true);
-	})();
+		});
 	i++;
-    }
+		}
+	clearInterval(timer);
+	}
+		setInterval(timer, 1000);
 }
 
 //pool.init(main);
 function main(){
-    console.log("main called");
+    //console.log("main called");
     process.nextTick(function(){
 	logica();
     });
@@ -62,6 +78,5 @@ function main(){
 }
 var config = {port : 22222, host : '127.0.0.1', minsize : 1, maxsize : 2, callback : main};
 var pool = poolmodule.pool(config);
-pool.init();
 
 //main();
--- test/roster.js
+++ test/roster.js
@@ -8,17 +8,23 @@
  *         the carrier component is comming from  https://github.com/pgte/carrier
  */
 var net = require('net');
-var carrier = require('./carrier');
+//var carrier = require('./carrier');
 
 (function(){
     var server = net.createServer(function(socket) {
 	socket.on('connect', function(){
 	    console.log('connect\n');
 	});
-	carrier.carry(socket, function(line){
+	socket.on('data', function(line){
 	    console.log('line:' + line);
 	    return socket.write("[Server Send]:" + line + '\n');
 	});
+	/*
+	carrier.carry(socket, function(line){
+	    console.log('line:' + line, server.connections.length);
+	    return socket.write("[Server Send]:" + line + '\n');
+	});
+	*/
     });
     server.listen(22222, function(){
 	console.log("server running at: tcp://0.22222");
